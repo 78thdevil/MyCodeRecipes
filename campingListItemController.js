0b14750127cf65a6d35d3b409586da14720a933c
Pruthvi Raj <pruthviraj301@gmail.com>
2020-06-21 21:17:43 +0530

    Add files via upload

---
+++ campingListItemController.js
@@ -0,0 +1,10 @@
+({
+    packItem: function(component, event, helper) {
+   /*     var action = component.get("v.item",true);
+        action.Packed__c = true;
+        component.set("v.item",action);
+       console.log('RRRRR'+action);
+         var btnClicked = event.getSource();
+        btnClicked.set("v.disabled",true);  
+   */ } 
+})
